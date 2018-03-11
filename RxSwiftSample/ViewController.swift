5041453b3f03bc81b9352fc647bf4196ef3bf254
KO-YAMAKO <ko.foot1103@gmail.com>
2018-03-11 14:56:25 +0900

    first commit

---
+++ RxSwiftSample/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RxSwiftSample
+//
+//  Created by 山浦功 on 2018/03/11.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
