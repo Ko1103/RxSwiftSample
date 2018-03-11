5a36bd7a7713976ab5dcb1491321a1ccd0bd5c9f
KO-YAMAKO <ko.foot1103@gmail.com>
2018-03-11 15:28:20 +0900

    make instancesx

--- RxSwiftSample/ViewModel.swift
+++ RxSwiftSample/ViewModel.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import RxSwift
+
+
+class ViewModel{
+    
+    let title = Variable<String>("")
+    let countTitle: Observable<Int>
+    let shouldSubmit: Observable<Bool>
+    
+    init() {
+        self.countTitle = self.title.asObservable().map({ text -> Int in
+            text.count
+        })
+        self.shouldSubmit = self.countTitle.map({ count -> Bool in
+            0 < count && count < 20
+        })
+    }
+}
